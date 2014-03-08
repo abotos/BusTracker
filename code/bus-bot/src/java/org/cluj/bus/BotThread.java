e9347535e2a5b69b27edb9e11ddd92a78bb9fdc9
abotos <anghel.botos@gmail.com>
2014-03-08 15:25:17 +0200

    Introduced BusBot

--- code/bus-bot/src/java/org/cluj/bus/BotThread.java
+++ code/bus-bot/src/java/org/cluj/bus/BotThread.java
@@ -10,12 +10,29 @@
  */
 package org.cluj.bus;
 
+import com.google.gson.Gson;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
+import org.cluj.bus.constants.Constants;
+import org.cluj.bus.pojo.BusLocation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BotThread extends Thread
 {
 
     private static final Logger LOGGER = Logger.getLogger(BotThread.class);
+
+    // TODO unhardcode this
+    private static final String SERVER_URI = "http://localhost:8080/bustracker/busUpdate";
 
     private static final int SLEEP_INTERVAL = 5000;
 
@@ -36,12 +53,43 @@ public class BotThread extends Thread
                 // TODO do something
                 LOGGER.info("BotThread is doing stuff");
 
+                BusLocation location = getNextLocation();
+
+                sendLocationUpdate(location);
+
                 Thread.sleep(SLEEP_INTERVAL);
             }
             catch (InterruptedException e)
             {
                 LOGGER.error("InterruptedException while sleeping", e);
             }
+            catch (IOException e)
+            {
+                LOGGER.error("IOException while sending update", e);
+            }
         }
+    }
+
+    private void sendLocationUpdate(BusLocation location) throws IOException
+    {
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(SERVER_URI);
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair(Constants.LOCATION_PARAMETER_KEY, new Gson().toJson(location)));
+        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        CloseableHttpResponse response = closeableHttpClient.execute(httpPost);
+
+        LOGGER.info("Got response [statusCode=" + response.getStatusLine().getStatusCode() + "]");
+    }
+
+    private BusLocation getNextLocation()
+    {
+        BusLocation location = new BusLocation();
+
+        location.setBusId("MyBus");
+        location.setxLat(123.5);
+        location.setyLat(22.6);
+
+        return location;
     }
 }
--- code/web-ui/src/java/org/cluj/bus/BusUpdateServlet.java
+++ code/web-ui/src/java/org/cluj/bus/BusUpdateServlet.java
@@ -10,49 +10,53 @@
  */
 package org.cluj.bus;
 
+import com.google.gson.Gson;
 import org.apache.log4j.Logger;
-import org.cluj.bus.db.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.cluj.bus.constants.Constants;
+import org.cluj.bus.pojo.BusLocation;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class BusUpdateServlet extends HttpServlet
 {
 
     private static final Logger LOGGER = Logger.getLogger(BusUpdateServlet.class);
+    private static final String BUS_LOCATION_MSG = "Bus location [busId=%s xLat=%f yLong=%f]";
 
     @Override
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException
     {
-        super.doPost(httpServletRequest, httpServletResponse);
+        String locationJson = httpServletRequest.getParameter(Constants.LOCATION_PARAMETER_KEY);
+
+        BusLocation busLocation = new Gson().fromJson(locationJson, BusLocation.class);
+
+        LOGGER.info(String.format(BUS_LOCATION_MSG, busLocation.getBusId(), busLocation.getxLat(), busLocation.getyLat()));
     }
 
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException
     {
-        Session session = HibernateUtil.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        LOGGER.info("Saving object");
+//        Session session = HibernateUtil.openSession();
+//        Transaction transaction = session.beginTransaction();
 //
-//        BusLocationUpdate newBusLocation = new BusLocationUpdate();
-//        newBusLocation.setName("Test");
+//        LOGGER.info("Saving object");
+////
+////        BusLocationUpdate newBusLocation = new BusLocationUpdate();
+////        newBusLocation.setName("Test");
+////
+////        session.saveOrUpdate(newBusLocation);
 //
-//        session.saveOrUpdate(newBusLocation);
-
-        List<BusLocationUpdate> list = session.createCriteria(BusLocationUpdate.class).list();
-        for (BusLocationUpdate busLocationUpdate : list)
-        {
-            LOGGER.info("BLU: [id=" + busLocationUpdate.getId() + " name=" + busLocationUpdate.getName() + "]");
-        }
-
-        transaction.commit();
-        session.close();
+//        List<BusLocationUpdate> list = session.createCriteria(BusLocationUpdate.class).list();
+//        for (BusLocationUpdate busLocationUpdate : list)
+//        {
+//            LOGGER.info("BLU: [id=" + busLocationUpdate.getId() + " name=" + busLocationUpdate.getName() + "]");
+//        }
+//
+//        transaction.commit();
+//        session.close();
     }
 }
