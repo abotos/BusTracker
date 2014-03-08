04c1dda7dbce3290dac8d2b7a9503465f33c9e99
abotos <anghel.botos@gmail.com>
2014-03-09 00:00:14 +0200

    Added populateBusStations.sql

--- resources/sql/dropSchema.sql
+++ resources/sql/dropSchema.sql
@@ -1,5 +1,14 @@
 --##################################################################### DROP_CONSTRAINTS ################################################
 
+begin execute immediate 'ALTER TABLE STATION_BUS DROP CONSTRAINT STATION_BUS_BUS_ID_FK'; exception when others then if sqlcode !=-2443 and sqlcode != -942 then raise; end if; end;
+/
+
+begin execute immediate 'ALTER TABLE STATION_BUS DROP CONSTRAINT STATION_BUS_STATION_ID_FK'; exception when others then if sqlcode !=-2443 and sqlcode != -942 then raise; end if; end;
+/
+
+begin execute immediate 'ALTER TABLE STATION_BUS DROP CONSTRAINT PK_STATION_BUS'; exception when others then if sqlcode !=-2443 and sqlcode != -942 then raise; end if; end;
+/
+
 begin execute immediate 'ALTER TABLE BUS DROP CONSTRAINT BUS_PK'; exception when others then if sqlcode !=-2443 and sqlcode != -942 then raise; end if; end;
 /
 
@@ -34,22 +43,26 @@ begin execute immediate 'ALTER TABLE STATION DROP CONSTRAINT UNIQUE_NAME'; excep
 /
 --##################################################################### DROP_TABLES ################################################
 
+begin execute immediate 'DROP TABLE TRIP'; exception when others then if sqlcode != -942 then raise; end if; end;
+/
 begin execute immediate 'DROP TABLE BUS'; exception when others then if sqlcode != -942 then raise; end if; end;
 /
 begin execute immediate 'DROP TABLE BUS_LOCATION_UPDATE'; exception when others then if sqlcode != -942 then raise; end if; end;
 /
-begin execute immediate 'DROP TABLE TRIP'; exception when others then if sqlcode != -942 then raise; end if; end;
-/
 begin execute immediate 'DROP TABLE STATION'; exception when others then if sqlcode != -942 then raise; end if; end;
+/
+begin execute immediate 'DROP TABLE STATION_BUS'; exception when others then if sqlcode != -942 then raise; end if; end;
 /
 
 --##################################################################### DROP_SEQUENCES ################################################
 
+begin execute immediate 'DROP SEQUENCE S_TRIP'; exception when others then if sqlcode != -2289 then raise; end if; end;
+/
 begin execute immediate 'DROP SEQUENCE S_BUS'; exception when others then if sqlcode != -2289 then raise; end if; end;
 /
 begin execute immediate 'DROP SEQUENCE S_BUS_LOCATION_UPDATE'; exception when others then if sqlcode != -2289 then raise; end if; end;
 /
-begin execute immediate 'DROP SEQUENCE S_TRIP'; exception when others then if sqlcode != -2289 then raise; end if; end;
-/
 begin execute immediate 'DROP SEQUENCE S_STATION'; exception when others then if sqlcode != -2289 then raise; end if; end;
+/
+begin execute immediate 'DROP SEQUENCE S_STATION_BUS'; exception when others then if sqlcode != -2289 then raise; end if; end;
 /
